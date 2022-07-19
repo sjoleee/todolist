2b30dbc44c8e8439eee5ce7ab96e8683ce74d4a1
sjoleee <cent7425@gmail.com>
2022-07-20 01:53:29 +0900

    todolist 만들기 위한 초기세팅

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "whitesmoke",
+  accentColor: "#00cec9",
+  cardColor: "#ffffff5a",
+};
