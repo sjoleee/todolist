0cf9e9dde2292af10902bb7e811df5a0accfee1b
sjoleee <cent7425@gmail.com>
2022-07-20 02:10:38 +0900

    delete app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
-import "./App.css";
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
