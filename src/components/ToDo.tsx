10e614c27446a98061b71030a49c1a513d0bdf37
sjoleee <cent7425@gmail.com>
2022-08-02 01:30:41 +0900

    todo 상태 변경하는 버튼 로직 만들기

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -8,7 +8,21 @@ function ToDo({ text, id, category }: IToDo) {
     const {
       currentTarget: { name },
     } = event;
-    console.log(event);
+    console.log(toDos);
+    //버튼 누르면 해당 버튼에 해당하는 상태(doing, done)로 변경되게 만들고 싶다.
+    setToDos((prev) => {
+      //결국 toDos를 변경해줘야하는거니까 setToDos를 쓰자.
+      const targetIdx = prev.findIndex((toDo) => toDo.id === id);
+      //toDos가 배열 안에 객체형태의 todo들이 모여있는거임. 그 중에서 내가 버튼을 클릭한 todo가 몇번째 todo인지 찾아내는 것.
+      //기존 toDos를 가져와서, findIndex, 그러니까 몇번째인지 찾는 매서드를 사용한다. findIndex는 인자로 함수를 받는데, 함수의 조건에 맞는게 몇번째였는지 찾아준다.
+      //이 경우에는 prev, 그러니까 기존 toDos에서 하나하나 투두들을 들여다보는데 버튼을 클릭한 그 toDo의 아이디와 돌고있는 toDo의 아이디가 같으면 그게 몇번째였는지 찾아주는거.
+      const oldToDo = prev[targetIdx];
+      const newToDo = { text, id, category: name };
+      //그렇게 targetIdx를 찾아내면, 이제 누른 버튼에 해당하는 상태로 변경해줘야한다.
+      //newToDo를 만들건데, text나 id는 유지하는데 category만 버튼의 name으로 변경해준다.
+      console.log(newToDo);
+      return prev;
+    });
   };
 
   return (
